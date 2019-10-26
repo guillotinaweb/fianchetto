2b8151dd2571804ff089a1ad1f1e11b472a306cb
Ferran Llamas <llamas.arroniz@gmail.com>
2019-10-26 17:19:53 +0200

    wip

--- fianchetto/fianchetto/__init__.py
+++ fianchetto/fianchetto/__init__.py
@@ -1,7 +1,13 @@
 from guillotina import configure
 
 app_settings = {
-    # provide custom application settings here...
+    "load_utilities": {
+        "fianchetto.chess_server_manager": {
+            "provides": "fianchetto.interfaces.IChessServerManager",
+            "factory": "fianchetto.utility.ChessServerManager",
+            "settings": {},
+        }
+    }
 }
 
 
@@ -10,6 +16,8 @@ def includeme(root):
     custom application initialization here
     """
     configure.scan("fianchetto.permissions")
+    configure.scan("fianchetto.interfaces")
     configure.scan("fianchetto.api")
+    configure.scan("fianchetto.utility")
     configure.scan("fianchetto.install")
     configure.scan("fianchetto.content")
--- fianchetto/fianchetto/api.py
+++ fianchetto/fianchetto/api.py
@@ -5,13 +5,53 @@ import aiohttp
 from aiohttp import web
 from guillotina import configure
 from guillotina.component import get_utility
+from guillotina.response import HTTPPreconditionFailed
 from guillotina.transactions import get_tm
+from guillotina.utils import get_authenticated_user_id
 
-from fianchetto.utility import IMessageSender
-
-from .content import IChessGame
+from fianchetto.content import IChessGame
+from fianchetto.interfaces import IChessServerManager
+from fianchetto.utility import ChessServerManager
 
 logger = logging.getLogger("fianchetto")
+
+
+@configure.service(
+    context=IChessGame,
+    method="POST",
+    name="@move",
+    permission="fianchetto.PlayGame",
+)
+async def move(context, request):
+    """
+    Endpoint to make moves
+    """
+    move: str = await request.text()
+    player_id: str = get_authenticated_user_id()
+    side: str = context.get_side(player_id)
+
+    move = translate_move(move)
+
+    if context.turn != side:
+        raise HTTPPreconditionFailed(content={"reason": "Not your move!"})
+
+    if not context.valid_move(move):
+        raise HTTPPreconditionFailed(content={"reason": "Invalid move"})
+
+    utility: ChessServerManager = get_utility(IChessServerManager)
+    await utility.queue_move(context, move, side=side)
+
+    # Append new move
+    moves = context.moves or []
+    moves.append(move)
+    context.moves = moves
+    context.register()
+
+
+def translate_move(move):
+    # TODO: translate from front-end move languate to the language we
+    # need to validate moves.
+    return move
 
 
 @configure.service(
@@ -21,9 +61,14 @@ logger = logging.getLogger("fianchetto")
     permission="fianchetto.PlayGame",
 )
 async def play_chess_game(context, request):
+    # Create new ws
     ws = web.WebSocketResponse()
-    utility = get_utility(IMessageSender)
-    utility.register_ws(ws, request)
+
+    # Get utility that manages game messages
+    utility = get_utility(IChessServerManager)
+    player_id = get_authenticated_user_id()
+
+    utility.register_player(ws, game=context, player_id=player_id)
 
     tm = get_tm()
     await tm.abort()
@@ -44,6 +89,6 @@ async def play_chess_game(context, request):
         pass
     finally:
         logger.debug("websocket connection closed")
-        utility.unregister_ws(ws)
+        utility.unregister_player(player_id)
 
     return {}
