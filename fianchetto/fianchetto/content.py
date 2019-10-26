4a2ae9edf768e8c70fd93b2a50b78dc7a813b022
Filippo Campi <filippo.campi@redturtle.it>
2019-10-26 12:28:31 +0200

    [dev] added ChessGame content type

--- fianchetto/fianchetto/content.py
+++ fianchetto/fianchetto/content.py
@@ -1,1 +1,19 @@
-# TODO: Create content type here
+from guillotina import configure, content, schema
+from guillotina.directives import index_field
+from guillotina.interfaces import IFolder
+
+
+class IChessGame(IFolder):
+
+    index_field("users", type="keyword")
+    white = schema.TextLine(title="White player", required=False)
+    black = schema.TextLine(title="Black player", required=False)
+    moves = schema.List(value_type=schema.TextLine(), default=list())
+
+
+@configure.contenttype(type_name="ChessGame", schema=IChessGame)
+class ChessGame(content.Folder):
+    """ ChessGame class """
+
+    def validate_move(self, move):
+        return True
